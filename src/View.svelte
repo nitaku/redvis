d185d935ac944f66714dc953f22b1c587bd0c404
nitaku <matteo.abrate@gmail.com>
2021-06-04 10:11:07 +0200

    cell style

--- src/View.svelte
+++ src/View.svelte
@@ -10,4 +10,11 @@
 		background: whitesmoke;
         flex-grow: 1;
 	}
+	main :global(.observablehq) {
+		margin: 4px;
+		padding: 4px;
+		background: white;
+		border-bottom: 1px solid #DDD;
+		border-radius: 4px;
+	}
 </style>
